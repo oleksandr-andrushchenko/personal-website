b13b9d04720673b50735905bdca44f1234befd72
oleksandr.andrushchenko1988@gmail.com <oleksandr.andrushchenko1988@gmail.com>
2025-11-22 00:08:58 -0700

    button links replaced with wrapped links with link icons; toggle details buttons moved on header lines; on print: hide images; seo: block h3 tags cleared; soft stylings improvements

--- src/static/scripts.js
+++ src/static/scripts.js
@@ -1,2 +1,22 @@
 const tooltipTriggerList = document.querySelectorAll("[data-bs-toggle=\"tooltip\"]")
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll(".btn-toggle").forEach(btn => {
+    const icon = btn.querySelector("i")
+    const targetSelector = btn.getAttribute("data-bs-target")
+    const target = document.querySelector(targetSelector)
+
+    if (!target) return
+
+    target.addEventListener("show.bs.collapse", () => {
+      icon.classList.remove("bi-chevron-down")
+      icon.classList.add("bi-chevron-up")
+    })
+
+    target.addEventListener("hide.bs.collapse", () => {
+      icon.classList.remove("bi-chevron-up")
+      icon.classList.add("bi-chevron-down")
+    })
+  })
+})
